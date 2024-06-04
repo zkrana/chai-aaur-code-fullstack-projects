917f850becbdcf2850c032a5f4cbe7719c0dac2b
Zk Rana <zkranao@gmail.com>
2024-06-04 15:35:01 +0600

    Email verification running

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,10 @@
+import nodemailer from "nodemailer";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const sendEmail = async ({ email, emailtype, userId }: any) => {
+  try {
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
